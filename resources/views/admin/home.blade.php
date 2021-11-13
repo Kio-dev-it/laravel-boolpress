7f8ea5e4ba477aa80f58b49ef65eeb3908a29837
Kio-dev-it <pas.petito@gmail.com>
2021-11-13 17:08:21 +0100

    Add base layout for back office

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('admin.layout.base')
 
+@section('pageContent')
     <h1>Boolpress</h1>
 
     <h2>Sei nel percorso admin/home</h2>
     <a href="{{route('admin.posts.index')}}">Vai a tutti i post</a>
-    
-</body>
-</html>
+@endsection
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,41 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-</head>
-<body>
+@extends('admin.layout.base')
 
+@section('pageContent')
     <h1 class="ml-5">Tutti i post</h1>
+        
+    <table class="table table-striped">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Author</th>
+                <th scope="col">Creation Date</th>
 
-    <div class="container">
-        <table class="table table-striped">
-            <thead class="thead-dark">
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($posts as $post)
                 <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Slug</th>
-                    <th scope="col">Author</th>
-                    <th scope="col">Creation Date</th>
-    
+                    <th scope="row">{{$post['id']}}</th>
+                    <td>{{$post['title']}}</td>
+                    <td>{{$post['slug']}}</td>
+                    <td>{{$post['author']}}</td>
+                    <td>{{$post['created_at']}}</td>
                 </tr>
-            </thead>
-            <tbody>
-                @foreach ($posts as $post)
-                    <tr>
-                        <th scope="row">{{$post['id']}}</th>
-                        <td>{{$post['title']}}</td>
-                        <td>{{$post['slug']}}</td>
-                        <td>{{$post['author']}}</td>
-                        <td>{{$post['created_at']}}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-    <script src="{{asset('js/app.js')}}"></script>
-</body>
-</html>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
+
+
