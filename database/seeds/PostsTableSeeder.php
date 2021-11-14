e0af563576b359d8cced82c8e69d0352df55fcbf
Kio-dev-it <pas.petito@gmail.com>
2021-11-14 21:41:05 +0100

    Change length of content in faker

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -19,7 +19,7 @@ class PostsTableSeeder extends Seeder
             $newPost = new Post();
             $newPost->title = $faker->words(7, true);
             $newPost->slug = Str::of($newPost->title)->slug('-');
-            $newPost->content = $faker->text();
+            $newPost->content = $faker->text(800);
             $newPost->author = $faker->firstName();
             $newPost->save();
         }
