707653396f407c472affcf0a5284a6cb03f174e8
Kio-dev-it <pas.petito@gmail.com>
2021-11-20 23:52:57 +0100

    add list of related post in category show

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -2,5 +2,17 @@
 
 @section('pageContent')
     <h1 class="mt-5">{{$category->name}}</h1>
+    <h3 class="mt-5">{{$category->slug}}</h3>
+    @if (count($category['posts']) > 0)
+        <h2 class="my-4">Lista post associati alla categoria:</h2>
+    @endif
+    @forelse ($category['posts'] as $post)
+        <ul>
+            <li><a href="{{route('admin.posts.show', $post['id'])}}">{{$post['title']}}</a></li>
+        </ul>
+    @empty
+        <h2 class="my-4"> Nessun post associato a questa categoria</h2>
+    @endforelse
+
 
 @endsection
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -2,7 +2,9 @@
 
 @section('pageContent')
     <h1 class="mt-5">{{$post->title}}</h1>
+    @if ($post['category'] != null)
     <h3>Categoria: {{$post['category']['name']}}</h3>
+    @endif
     <h5>{{$post->slug}}</h5>
     <p>{{$post->content}}</p>
     <span>{{$post->created_at}} da <em>{{$post->author}}</em></span>
