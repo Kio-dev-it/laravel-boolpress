82da959bfe3b4b305ffd73d5942f54be3c3b0698
Kio-dev-it <pas.petito@gmail.com>
2021-11-25 00:08:21 +0100

    Add tags in post show page on backoffice

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -5,6 +5,12 @@
     @if ($post['category'] != null)
     <h3>Categoria: {{$post['category']['name']}}</h3>
     @endif
+    @if($post['tags'] != null)
+    <h3>Tag:</h3>
+        @foreach ($post['tags'] as $tag)
+            <span class="badge badge-success">{{$tag['name']}}</span>
+        @endforeach
+    @endif
     <h5>{{$post->slug}}</h5>
     <p>{{$post->content}}</p>
     <span>{{$post->created_at}} da <em>{{$post->author}}</em></span>
